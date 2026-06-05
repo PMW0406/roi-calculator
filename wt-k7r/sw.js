8305f3847849a572cad7f8a721d14353190663e2
PMW0406 <PMW0406@users.noreply.github.com>
2026-06-05 10:14:07 +0900

    한국어 wt-k7r/로 이동, 루트 빈 페이지 처리

---
+++ wt-k7r/sw.js
@@ -0,0 +1,32 @@
+const CACHE = "wontech-roi-ko-v1";
+const ASSETS = [
+  "/roi-calculator/wt-k7r/",
+  "/roi-calculator/wt-k7r/index.html",
+  "/roi-calculator/styles.css",
+  "/roi-calculator/lang.js",
+  "/roi-calculator/speech.js",
+  "/roi-calculator/app.js",
+  "/roi-calculator/KakaoTalk_20260413_161622234_01.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => { const c = res.clone(); caches.open(CACHE).then((ca) => ca.put(e.request, c)); return res; })
+      .catch(() => caches.match(e.request))
+  );
+});
